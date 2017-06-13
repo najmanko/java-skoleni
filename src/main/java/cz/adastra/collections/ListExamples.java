1d7045494148a4ac06e6162fa017787c910dbb2f
jozef.najman <jozef.najman@4finance.com>
2017-06-13 15:51:39 +0200

    array examples added

--- src/main/java/cz/adastra/collections/ListExamples.java
+++ src/main/java/cz/adastra/collections/ListExamples.java
@@ -1,4 +1,49 @@
 package cz.adastra.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListExamples {
+
+    public static void main(String[] args) {
+        ListExamples examples = new ListExamples();
+        examples.oldUseOfListUnderJava5();
+
+        examples.newUsageAfterJava5();
+    }
+
+    private void oldUseOfListUnderJava5() {
+        List arrayList = new ArrayList();
+        arrayList.add(7);
+        arrayList.add("Ahoj");
+        arrayList.add(false);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            Object o = arrayList.get(i);
+            System.out.print("Nasel jsem ");
+            if (o instanceof Integer) {
+                int number = (Integer)o;
+                System.out.println("Integer: " + number);
+            } else if (o instanceof String) {
+                String slovo = (String)o;
+                System.out.println("String: " + slovo);
+            } else if (o instanceof Boolean) {
+                boolean hodnota = (Boolean)o;
+                System.out.println("Boolean: " + hodnota);
+            }
+            System.out.println(o);
+        }
+    }
+
+    private void newUsageAfterJava5() {
+        List<Integer> integerList = new ArrayList<Integer>();
+
+        integerList.add(6);
+        integerList.add(-10);
+        integerList.add(Integer.MAX_VALUE);
+
+        for (Integer i : integerList) {
+            System.out.println("Cislo: " + i);
+        }
+    }
 }
