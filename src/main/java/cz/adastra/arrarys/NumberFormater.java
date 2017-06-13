4d74aee34121ad5dd935206428da933dbffcf632
jozef.najman <jozef.najman@4finance.com>
2017-06-13 17:07:13 +0200

    number formatter

--- src/main/java/cz/adastra/arrarys/NumberFormater.java
+++ src/main/java/cz/adastra/arrarys/NumberFormater.java
@@ -3,25 +3,20 @@ package cz.adastra.arrarys;
 public class NumberFormater {
 
     public static void main(String[] args) {
-        long number = 987654321;
+        long number = 9654321;
 
         String numString = String.valueOf(number);
         int tmp = 0;
         String tmpString = "";
 
         for (int i = numString.length() - 1; i >= 0; i--) {
-            if (tmp % 3 == 0) {
-                tmpString = tmpString + " ";
+            if (tmp > 0 && tmp % 3 == 0) {
+                tmpString = " " + tmpString;
             }
-            tmpString = tmpString + numString.charAt(i);
+            tmpString = numString.charAt(i) + tmpString;
             ++tmp;
         }
 
-        System.out.println(tmpString);
-        StringBuilder builder = new StringBuilder();
-        builder.append(tmpString);
-        builder.reverse();
-
-        System.out.println("Cislo: " + builder.toString());
+        System.out.println("Cena: " + tmpString + " Kc");
     }
 }
