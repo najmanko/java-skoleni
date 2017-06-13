cea57ee9ebc7f7828cf517dd174d008fba911fbf
jozef.najman <jozef.najman@4finance.com>
2017-06-13 15:59:41 +0200

    array sort begining added

---
+++ src/main/java/cz/adastra/arrarys/ArraySorting.java
@@ -0,0 +1,19 @@
+package cz.adastra.arrarys;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ArraySorting {
+
+    public static void main(String[] args) {
+        ArraySorting arraySorting = new ArraySorting();
+
+        List list = arraySorting.generateArray(10);
+
+        Collections.sort(list);
+    }
+
+    private List<Integer> generateArray(int size) {
+        return null;
+    }
+}
