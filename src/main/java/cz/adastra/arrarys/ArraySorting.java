3d1a2ccb3988115ff9d76f8391b85c827173c8ef
jozef.najman <jozef.najman@4finance.com>
2017-06-13 16:15:58 +0200

    fixed java 8 in maven + arraysort done

--- src/main/java/cz/adastra/arrarys/ArraySorting.java
+++ src/main/java/cz/adastra/arrarys/ArraySorting.java
@@ -1,19 +1,39 @@
 package cz.adastra.arrarys;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class ArraySorting {
 
     public static void main(String[] args) {
         ArraySorting arraySorting = new ArraySorting();
 
-        List list = arraySorting.generateArray(10);
+        List<Integer> list = arraySorting.generateArray(10);
 
         Collections.sort(list);
+
+        System.out.println("Sorted:");
+        for (Integer i : list) {
+            System.out.print(i + ", ");
+        }
     }
 
     private List<Integer> generateArray(int size) {
-        return null;
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(100));
+        }
+
+        System.out.println("Unsorted:");
+        for (Integer i : list) {
+            System.out.print(i + ", ");
+        }
+
+        System.out.println();
+        return list;
     }
 }
