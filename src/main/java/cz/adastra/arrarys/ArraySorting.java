64bf0ba13a879d75ec1de6479db0be94d8c835f6
jozef.najman <jozef.najman@4finance.com>
2017-06-13 16:34:17 +0200

    array sorting

--- src/main/java/cz/adastra/arrarys/ArraySorting.java
+++ src/main/java/cz/adastra/arrarys/ArraySorting.java
@@ -1,9 +1,6 @@
 package cz.adastra.arrarys;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ArraySorting {
 
@@ -12,9 +9,21 @@ public class ArraySorting {
 
         List<Integer> list = arraySorting.generateArray(10);
 
-        Collections.sort(list);
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        });
 
         System.out.println("Sorted:");
+        for (Integer i : list) {
+            System.out.print(i + ", ");
+        }
+
+        Collections.sort(list, (o1, o2) -> o2.compareTo(o1));
+
+        System.out.println("\nSorted backward:");
         for (Integer i : list) {
             System.out.print(i + ", ");
         }
