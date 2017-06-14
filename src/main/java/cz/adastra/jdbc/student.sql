4c4d3a2b84f9e99f0fd477da511238fdebcb1a4f
jozef.najman <jozef.najman@4finance.com>
2017-06-14 09:10:23 +0200

    added SQL inserts

--- src/main/java/cz/adastra/jdbc/student.sql
+++ src/main/java/cz/adastra/jdbc/student.sql
@@ -1,8 +1,11 @@
 CREATE TABLE STUDENT (
          ID           NUMBER(5) PRIMARY KEY,
-         FIRST_NAME   VARCHAR2(15) NOT NULL,
-         LAST_NAME    VARCHAR2(20) NOT NULL,
+         NAME         VARCHAR2(15) NOT NULL,
+         SURNAME      VARCHAR2(20) NOT NULL,
          AGE          NUMBER(3)
          );
 
-INSERT INTO 
+INSERT INTO STUDENT VALUES (1, 'Jan', 'Nepomuk', 387);
+INSERT INTO STUDENT VALUES (2, 'Jara', 'Cimrman', 100);
+INSERT INTO STUDENT VALUES (1, 'Milos ', 'Zeman', 70);
+INSERT INTO STUDENT VALUES (1, 'Petr', 'Maly', 5);
