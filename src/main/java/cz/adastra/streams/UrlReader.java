cb8b7083546b8f0a3809ad499f8b1e351a62f925
jozef.najman <jozef.najman@4finance.com>
2017-06-14 14:39:37 +0200

    URL reader added encoding

--- src/main/java/cz/adastra/streams/UrlReader.java
+++ src/main/java/cz/adastra/streams/UrlReader.java
@@ -3,12 +3,16 @@ package cz.adastra.streams;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 public class UrlReader {
     public static void main(String[] args) throws Exception {
 
         URL idnes = new URL("http://www.idnes.cz");
-        BufferedReader in = new BufferedReader(new InputStreamReader(idnes.openStream()));
+        BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(
+                                idnes.openStream(), "windows-1250"));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null)
